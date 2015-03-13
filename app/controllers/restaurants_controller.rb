fe0663ae2d72a6c95d5a7c52033a5c181c2fefcb
gordonmotsinger <gordon.motsinger@gmail.com>
2015-03-13 12:17:24 -0400

    fixed update and added delete function

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,7 +12,8 @@ class RestaurantsController < ApplicationController
         @restaurant = Restaurant.find(params[:id])
         @map = 'https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400'
         @map_dress = @restaurant.address.gsub(/ /, '+')        
-        @map1 = 'https://maps.googleapis.com/maps/api/staticmap?center=' + @map_dress.to_s + '&zoom=14&size=400x400'
+        @map1 = 'https://maps.googleapis.com/maps/api/staticmap?center=' + @map_dress.to_s + '&zoom=14&size=400x400&
+markers=color:blue'+@map_dress.to_s
 
     end
 
@@ -30,8 +31,23 @@ class RestaurantsController < ApplicationController
         @restaurant = Restaurant.find(params[:id]) #find the item
     end
 
-    def update
-    end
+def update
+  @restaurant = Restaurant.find(params[:id])
+ 
+  if @restaurant.update(restaurant_params)
+    redirect_to @restaurant
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+    @restaurant=Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to root_path
+    flash[:notice] = "You didn't see a cat, did you?"
+end
+
 
     private
     def restaurant_params
