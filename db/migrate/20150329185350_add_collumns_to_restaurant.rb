402cb525e37373070924fad3d49fbca306d301a8
gordonmotsinger <gordon.motsinger@gmail.com>
2015-03-29 14:58:46 -0400

    added gender and num_bed to table Restaurant for it to function with data about homeless shelters

---
+++ db/migrate/20150329185350_add_collumns_to_restaurant.rb
@@ -0,0 +1,4 @@
+class AddCollumnsToRestaurant < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325192952) do
+ActiveRecord::Schema.define(version: 20150329185350) do
 
   create_table "reservations", force: :cascade do |t|
     t.string   "res_email"
