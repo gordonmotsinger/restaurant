f7c6ce4855193ead9ac15bf510fd4036c9151e77
gordonmotsinger <gordon.motsinger@gmail.com>
2015-04-10 12:33:07 -0400

    switched login to logout for delete route

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   get     'login' => 'sessions#new'
   post    'login' => 'sessions#create'
-  delete  'login' => 'sessions#delete'
+  delete  'logout' => 'sessions#delete'
 
   root :to => "restaurants#index"
 
